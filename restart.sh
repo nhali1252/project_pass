9fddf69a08f7fd0bda5befb012e8847a7f3d0eb7
nhali1252 <124444223+nhali1252@users.noreply.github.com>
2026-08-19 03:04:49 +0600

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -2,7 +2,7 @@
 echo "🔄 Killing existing cloudflared instances..."
 pkill -f cloudflared
 echo "🔄 Starting Cloudflare Tunnel in background..."
-nohup /usr/bin/cloudflared tunnel run --token eyJhIjoiZDRmNjVkMDQyNTZjMWRjNzY4ZGI3YmY2NWQzODE0NTEiLCJ0IjoiMWY3MDI0ZGEtZmJhMy00ZjkxLWEzMWEtZWZmOWFmN2I3NTg0IiwicyI6Ik56ZG1NbVZoWW1RdFpqZGxNaTAwWkdReExUaG1OemN0W1mFeU1tWmxOakl5TVRjMCJ9 > tunnel.log 2>&1 &
+nohup /usr/bin/cloudflared tunnel run --token eyJhIjoiZDRmNjVkMDQyNTZjMWRjNzY4ZGI3YmY2NWQzODE0NTEiLCJ0IjoiZWUxNmVkYjUtMjE1MC00YzQxLWE1NTktMDE1YjA5MjkyOTYxIiwicyI6Ik16VTVaamd3T0dZdE1UYzNZaTAwWmpka0xXRTNNMlV0WlRNNU0yWmpZamRpTlRFeiJ9 > tunnel.log 2>&1 &
 echo "🔄 Restarting Python Server..."
 pkill -f server.py
 nohup python3 server.py > server.log 2>&1 &
